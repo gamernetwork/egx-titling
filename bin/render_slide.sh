48229ac7f079d647fcca9c94831975c80354f346
Mark Kennedy <mark@gamer-network.net>
2015-03-11 12:33:59 +0000

    fixed render script

--- bin/render_slide.sh
+++ bin/render_slide.sh
@@ -1,7 +1,48 @@
+#!/bin/bash
+
+DURATION=150
+#FILENAME=EGX`date +%Y`_`date +%a_%T`_$SHOT.ts
+
+usage()
+{
+cat << EOF
+usage: $0 options <template filename> <output filename>
+
+Renders egx titling templates to Prores
+
+OPTIONS:
+   -h      Show this message
+   -d      Duration of render in seconds (default=150)
+EOF
+}
+
+while getopts “d:” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         d)
+             DURATION=$OPTARG
+		 	 echo "Rendering a $DURATION second clip\n"
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+shift $((OPTIND-1))
+
+echo "Using template $1"
+echo "Using output $2"
+
 LD_LIBRARY_PATH=/usr/local/lib/ \
 	melt \
 	webvfx:$1 \
-	length=50 \
+	length=$DURATION \
 	speaker="Mark Kennedy" \
 	company="Gamer Network Ltd" \
 	transparent=1 \
@@ -15,5 +56,5 @@ LD_LIBRARY_PATH=/usr/local/lib/ \
 	transparent=1 \
 	pix_fmt=yuva444p10le \
 	alpha_bits=8 \
-	profile:v=4444 \
+	profile=4444 \
 	vcodec=prores
