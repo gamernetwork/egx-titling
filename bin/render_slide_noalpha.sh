e84c37383d6b03b9268bc65d558527f80a65cb3b
Mark Kennedy <mark@gamer-network.net>
2015-03-12 10:39:41 +0000

    simpler mp4 slide render

---
+++ bin/render_slide_noalpha.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+DURATION=150
+#FILENAME=EGX`date +%Y`_`date +%a_%T`_$SHOT.ts
+
+usage()
+{
+cat << EOF
+usage: $0 options <template filename> <output filename>
+
+Renders egx titling templates to Prores
+
+OPTIONS:
+   -h      Show this message
+   -d      Duration of render in seconds (default=150)
+EOF
+}
+
+while getopts “d:” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         d)
+             DURATION=$OPTARG
+		 	 echo "Rendering a $DURATION second clip\n"
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+shift $((OPTIND-1))
+
+echo "Using template $1"
+echo "Using output $2"
+
+LD_LIBRARY_PATH=/usr/local/lib/ \
+	melt \
+	webvfx:$1 \
+	length=$DURATION \
+	speaker="Mark Kennedy" \
+	company="Gamer Network Ltd" \
+	-consumer avformat:$2 \
+	no_audio=1 \
+	frame_rate_num=30000 \
+	frame_rate_den=1001 \
+	width=1920 \
+	height=1080
