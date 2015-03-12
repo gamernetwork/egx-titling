f7f8a059d327a8c45d4695cd4f560cbbeac99937
Mark Kennedy <mark@gamer-network.net>
2015-03-12 23:45:21 +0000

    batch files for rendering everything

---
+++ bin/render_single_slide.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+DURATION=2
+#FILENAME=EGX`date +%Y`_`date +%a_%T`_$SHOT.ts
+
+usage()
+{
+cat << EOF
+usage: $0 options <template filename> <output filename> [var=val ...]
+
+Renders egx titling templates to Prores
+
+OPTIONS:
+   -h      Show this message
+   -d      Duration of render in seconds (default=150)
+EOF
+}
+
+while getopts “d:” OPTION
+do
+     case $OPTION in
+         h)
+             usage
+             exit 1
+             ;;
+         d)
+             DURATION=$OPTARG
+		 	 echo "Rendering a $DURATION second clip\n"
+             ;;
+         ?)
+             usage
+             exit
+             ;;
+     esac
+done
+
+shift $((OPTIND-1))
+
+template=$1
+output=$2
+echo "Using template $template"
+echo "Using output $output"
+
+# pop off everything but custom params
+shift $((2))
+
+LD_LIBRARY_PATH=/usr/local/lib/ \
+	melt \
+	webvfx:$template \
+	length=$DURATION \
+	"$@" \
+	transparent=1 \
+	-consumer avformat:$output \
+	vcodec=qtrle \
+	no_audio=1 \
+	mlt_image_format=rgb24a \
+	frame_rate_num=30000 \
+	frame_rate_den=1001 \
+	width=1920 \
+	height=1080 \
+	transparent=1 \
+	pix_fmt=argb
--- render_all_lt_next.sh
+++ render_all_lt_next.sh
@@ -2,100 +2,43 @@
 
 OUTPUT_DIR=~/Videos/egx/
 
-mkdir -p  $OUTPUT_DIR/friday/slates/
-mkdir -p  $OUTPUT_DIR/saturday/slates/
+mkdir -p  $OUTPUT_DIR/friday/lowerthirds/
+mkdir -p  $OUTPUT_DIR/saturday/lowerthirds/
 
-./bin/render_slide_noalpha.sh -d 180 templates/slates/full_schedule.webvfx.html $OUTPUT_DIR/friday/slates/full_schedule.mp4 \
-	t1="13:00 - 13:40" s1="Virtual Reality in 2015" d1="Valve's Chet Faliszek looks forward to creating content for 2015 and beyond." \
-	t2="14:00 - 14:40" s2="Total War" d2="15 years of battle animation in Total War with Developers from Creative Assembly." \
-	t3="15:00 - 15:40" s3="The Swindle" d3="Dan Marshall talks stealth game design decisions." \
-	t4="16:00 - 16:40" s4="Titan Souls" d4="Mark Foster and David Fenn from Devolver Digital on the origins of their team and the design philosophy" \
-	t5="17:00 - 17:40" s5="LA Cops" d5="Modern Dream on the fast, action packed 3D top down shooter." \
-	day="FRIDAY"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/friday/lowerthirds/now_1300.mov \
+	name="Virtual Reality in 2015" info="Valve's Chet Faliszek looks forward to creating content for 2015 and beyond." \
+	title="NOW:"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/friday/lowerthirds/now_1400.mov \
+	name="Total War" info="15 years of battle animation in Total War with Developers from Creative Assembly." \
+	title="NOW:"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/friday/lowerthirds/now_1500.mov \
+	name="The Swindle" info="Dan Marshall talks stealth game design decisions." \
+	title="NOW:"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/friday/lowerthirds/now_1600.mov \
+	name="Titan Souls" info="Mark Foster and David Fenn from Devolver Digital on the origins of their team and the design philosophy" \
+	title="NOW:"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/friday/lowerthirds/now_1700.mov \
+	name="LA Cops" info="Modern Dream on the fast, action packed 3D top down shooter." \
+	title="NOW:"
 
-./bin/render_slide_noalpha.sh -d 180 templates/slates/full_schedule.webvfx.html $OUTPUT_DIR/saturday/slates/full_schedule.mp4 \
-	t1="13:00 - 13:40" s1="Torment: Tides Of Numenera" d1="Colin McComb discusses developing for player experience and perception." \
-	t2="14:00 - 14:40" s2="Life Is Strange 2" d2="Dontnod Entertainment on what to expect from the next installment of the five part adventure game." \
-	t3="15:00 - 15:40" s3="Guild Wars 2: Heart Of Thorns" d3="Designers from ArenaNet walkthrough the key features and reveal of the massive open world combat." \
-	t4="16:00 - 16:40" s4="Rebirth Of The 3D Platformer" d4="Playtonic Games detail plans to resurrect the 3D platformer." \
-	t5="17:00 - 17:40" s5="Creative Assembly Game Jam" d5="Individuals & teams create a game in a day and have it reviewed by a panel of games industry professionals." \
-	day="SATURDAY"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/saturday/lowerthirds/next_1300.mov \
+	name="Torment: Tides Of Numenera" info="Colin McComb discusses developing for player experience and perception." \
+	name="NEXT:"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/saturday/lowerthirds/next_1400.mov \
+	name="Life Is Strange 2" info="Dontnod Entertainment on what to expect from the next installment of the five part adventure game." \
+	name="NEXT:"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/saturday/lowerthirds/next_1500.mov \
+	name="Guild Wars 2: Heart Of Thorns" info="Designers from ArenaNet walkthrough the key features and reveal of the massive open world combat." \
+	name="NEXT:"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/saturday/lowerthirds/next_1600.mov \
+	name="Rebirth Of The 3D Platformer" info="Playtonic Games detail plans to resurrect the 3D platformer." \
+	name="NEXT:"
+./bin/render_single_slide.sh templates/lower_thirds/lt.webvfx.html $OUTPUT_DIR/saturday/lowerthirds/next_1700.mov \
+	name="Creative Assembly Game Jam" info="Individuals & teams create a game in a day and have it reviewed by a panel of games industry professionals." \
+	name="NEXT:"
 
-./bin/render_slide_noalpha.sh -d 180 templates/slates/full_schedule.webvfx.html $OUTPUT_DIR/friday/slates/tomorrow.mp4 \
-	t1="13:00 - 13:40" s1="Torment: Tides Of Numenera" d1="Colin McComb discusses developing for player experience and perception." \
-	t2="14:00 - 14:40" s2="Life Is Strange 2" d2="Dontnod Entertainment on what to expect from the next installment of the five part adventure game." \
-	t3="15:00 - 15:40" s3="Guild Wars 2: Heart Of Thorns" d3="Designers from ArenaNet walkthrough the key features and reveal of the massive open world combat." \
-	t4="16:00 - 16:40" s4="Rebirth Of The 3D Platformer" d4="Playtonic Games detail plans to resurrect the 3D platformer." \
-	t5="17:00 - 17:40" s5="Creative Assembly Game Jam" d5="Individuals & teams create a game in a day and have it reviewed by a panel of games industry professionals." \
-	day="COME BACK TOMORROW FOR"
-
-#fri 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/friday/slates/now_1300.mp4 \
-	sessionName="Virtual Reality in 2015" sessionInfo="Valve's Chet Faliszek looks forward to creating content for 2015 and beyond." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/friday/slates/now_1400.mp4 \
-	sessionName="Total War" sessionInfo="15 years of battle animation in Total War with Developers from Creative Assembly." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/friday/slates/now_1500.mp4 \
-	sessionName="The Swindle" sessionInfo="Dan Marshall talks stealth game design decisions." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/friday/slates/now_1600.mp4 \
-	sessionName="Titan Souls" sessionInfo="Mark Foster and David Fenn from Devolver Digital on the origins of their team and the design philosophy" 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/friday/slates/now_1700.mp4 \
-	sessionName="LA Cops" sessionInfo="Modern Dream on the fast, action packed 3D top down shooter." 
-
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/friday/slates/next_1300.mp4 \
-	sessionName="Virtual Reality in 2015" sessionInfo="Valve's Chet Faliszek looks forward to creating content for 2015 and beyond." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/friday/slates/next_1400.mp4 \
-	sessionName="Total War" sessionInfo="15 years of battle animation in Total War with Developers from Creative Assembly." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/friday/slates/next_1500.mp4 \
-	sessionName="The Swindle" sessionInfo="Dan Marshall talks stealth game design decisions." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/friday/slates/next_1600.mp4 \
-	sessionName="Titan Souls" sessionInfo="Mark Foster and David Fenn from Devolver Digital on the origins of their team and the design philosophy" 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/friday/slates/next_1700.mp4 \
-	sessionName="LA Cops" sessionInfo="Modern Dream on the fast, action packed 3D top down shooter." 
-
-
-#sat
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/saturday/slates/now_1300.mp4 \
-	sessionName="Torment: Tides Of Numenera" sessionInfo="Colin McComb discusses developing for player experience and perception." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/saturday/slates/now_1400.mp4 \
-	sessionName="Life Is Strange 2" sessionInfo="Dontnod Entertainment on what to expect from the next installment of the five part adventure game." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/saturday/slates/now_1500.mp4 \
-	sessionName="Guild Wars 2: Heart Of Thorns" sessionInfo="Designers from ArenaNet walkthrough the key features and reveal of the massive open world combat." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/saturday/slates/now_1600.mp4 \
-	sessionName="Rebirth Of The 3D Platformer" sessionInfo="Playtonic Games detail plans to resurrect the 3D platformer." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/now.webvfx.html $OUTPUT_DIR/saturday/slates/now_1700.mp4 \
-	sessionName="Creative Assembly Game Jam" sessionInfo="Individuals & teams create a game in a day and have it reviewed by a panel of games industry professionals." 
-
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/saturday/slates/next_1300.mp4 \
-	sessionName="Torment: Tides Of Numenera" sessionInfo="Colin McComb discusses developing for player experience and perception." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/saturday/slates/next_1400.mp4 \
-	sessionName="Life Is Strange 2" sessionInfo="Dontnod Entertainment on what to expect from the next installment of the five part adventure game." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/saturday/slates/next_1500.mp4 \
-	sessionName="Guild Wars 2: Heart Of Thorns" sessionInfo="Designers from ArenaNet walkthrough the key features and reveal of the massive open world combat." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/saturday/slates/next_1600.mp4 \
-	sessionName="Rebirth Of The 3D Platformer" sessionInfo="Playtonic Games detail plans to resurrect the 3D platformer." 
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/next.webvfx.html $OUTPUT_DIR/saturday/slates/next_1700.mp4 \
-	sessionName="Creative Assembly Game Jam" sessionInfo="Individuals & teams create a game in a day and have it reviewed by a panel of games industry professionals." 
-
-
-./bin/render_slide_noalpha.sh -d 180 templates/slates/join_us_for_egx.webvfx.html $OUTPUT_DIR/saturday/slates/join_us_for_egx.mp4
-
+for file in $OUTPUT_DIR/{friday,saturday}/lowerthirds/*.mov;
+do
+	avconv -ss 5 -r 1 -i $file -frames 1 ${file/.mov/.png}
+	rm $file
+done;
