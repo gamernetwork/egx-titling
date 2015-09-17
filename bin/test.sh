9d9e4632f79ca01b52acd09687292467b46fe7ec
Craig Munro <craig@craigmunro.net>
2015-09-17 09:43:16 +0100

    Tidying up

--- bin/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-LD_LIBRARY_PATH=/usr/local/lib melt webvfx:/home/mark/Documents/Work/egx/2015/sessions/titling/poc.html width=1920 height=1080 length=300 progressive=0 -consumer sdl audio_off=1 frame_rate_num=60 frame_rate_den=1 width=960 height=540 display_aspect_num=1920 display_aspect_den=1080 progressive=0 sample_aspect_num=1 sample_aspect_den=1 terminate_on_pause=1
