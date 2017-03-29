594d4df2c5829c7fec9c5bce3cc5510e291bf515
Sam Hayes <sam.hayes@gamer-network.net>
2017-03-29 23:43:58 +0100

    Change schedule to load once only, add end of day slate

---
+++ schedule/rezzed-2017.js
@@ -0,0 +1,130 @@
+var sessions = [
+    {
+        "day": "Thursday",
+        "timestart": "12:00",
+        "timeend": "12:30",
+        "heading": "Ken Levine – A Conversation",
+        "subheading": "BioShock creator, Ken Levine, speaks with Eurogamer’s Oli Welsh about his love for games, his time as a game designer, and Ghost Story Games.",
+        "image": "rezzed-2017/ken-levine.jpg"
+    },
+    {
+        "day": "Thursday",
+        "timestart": "13:00",
+        "timeend": "13:30",
+        "heading": "Prey and the Arkane Factor",
+        "subheading": "Join Creative Director Raphael Colantonio, as he showcases new Prey gameplay and discusses what makes the immersive sim such a unique and exciting genre.",
+        "image": "rezzed-2017/prey.jpg"
+    },
+    {
+        "day": "Thursday",
+        "timestart": "14:00",
+        "timeend": "14:30",
+        "heading": "Divinity: Original Sin 2",
+        "subheading": "Swen Vincke (founder of Larian Studios) and his panel will address the challenges of building fully-fledged narratives that play nice within the main story when the characters themselves don't.",
+        "image": "rezzed-2017/divinity.jpg"
+    },
+    {
+        "day": "Thursday",
+        "timestart": "15:00",
+        "timeend": "15:30",
+        "heading": "Knights and Bikes with Rock, Paper, Shotgun",
+        "subheading": "Foam Sword, developers behind the charming and energetic eighties-inspired Knights and Bikes, join RPS on stage.",
+        "image": "rezzed-2017/knights-and-bikes.jpg"
+    },
+    {
+        "day": "Thursday",
+        "timestart": "16:00",
+        "timeend": "16:30",
+        "heading": "William Pugh",
+        "subheading": "William Pugh (founder of Crows Crows Crows and Designer on The Stanley Parable) tells stories from navigating the games industry and avoiding death at the hand of publishers.",
+        "image": "rezzed-2017/dr-langeskov.jpg"
+    },
+    {
+        "day": "Friday",
+        "timestart": "12:00",
+        "timeend": "12:30",
+        "heading": "Cities: Skylines with Rock, Paper, Shotgun",
+        "subheading": "Karoliina Korppoo, Lead Designer at Colossal Order, talks about the continuous development of Cities: Skylines, and working alongside a community.",
+        "image": "rezzed-2017/cities-skylines.jpg"
+    },
+    {
+        "day": "Friday",
+        "timestart": "13:00",
+        "timeend": "13:30",
+        "heading": "The Future of Gaming Tech",
+        "subheading": "Join the Digital Foundry team as they discuss 4K and the future of PC and console technology.",
+        "image": "rezzed-2017/horizon.jpg"
+    },
+    {
+        "day": "Friday",
+        "timestart": "14:00",
+        "timeend": "14:30",
+        "heading": "Steel Division: Normandy 44",
+        "subheading": "Creative Director Alexis Le Dressay takes to the stage to talk about the challenges of developing within the bounds of history, and the thrill of a well executed battle plan.",
+        "image": "rezzed-2017/steel-division.jpg"
+    },
+    {
+        "day": "Friday",
+        "timestart": "15:00",
+        "timeend": "15:30",
+        "heading": "Taking to the Sunless Skies",
+        "subheading": "Failbetter Games discuss the sequel to Sunless Sea, which was 377% funded on Kickstarter! Lottie Bevan, Cash DeCuir and Liam Welton in conversation with Eurogamer.",
+        "image": "rezzed-2017/sunless-skies.jpg"
+    },
+    {
+        "day": "Friday",
+        "timestart": "16:00",
+        "timeend": "16:30",
+        "heading": "The future of Total War: WARHAMMER",
+        "subheading": "Join developers from Creative Assembly for an exclusive look at what's next for the Total War: WARHAMMER series. Fans won't want to miss this!",
+        "image": "rezzed-2017/total-warhammer.jpg"
+    },
+    {
+        "day": "Saturday",
+        "timestart": "12:00",
+        "timeend": "12:30",
+        "heading": "Dean Hall from RocketWerkz",
+        "subheading": "Dean Hall, the original creator of DayZ and founder of RocketWerkz, goes in-depth on the studio's new game and takes questions from the audience.",
+        "image": "rezzed-2017/rocketwerkz.jpg"
+    },
+    {
+        "day": "Saturday",
+        "timestart": "13:00",
+        "timeend": "13:30",
+        "heading": "Battalion 1944",
+        "subheading": "Senior Producer and Designer on Battalion Joe Brammer will be discussing what \"old school\" really is when it comes to shooters and he'll be looking to audience for ideas, features and requests.",
+        "image": "rezzed-2017/battalion.jpg"
+    },
+    {
+        "day": "Saturday",
+        "timestart": "14:00",
+        "timeend": "14:30",
+        "heading": "Xenonauts-2",
+        "subheading": "Chris England from Goldhawk Interactive joins Eurogamer's Chris \"Strategy King\" Bratt to chat about the latest Xenonauts-2 developments.",
+        "image": "rezzed-2017/xenonauts.jpg"
+    },
+    {
+        "day": "Saturday",
+        "timestart": "15:00",
+        "timeend": "15:30",
+        "heading": "Yooka-Laylee",
+        "subheading": "Join developers from Playtonic as they play through the opening stages of Yooka-Laylee in a worldwide exclusive session. This is the only chance you'll have to see the final version of Yooka-Laylee before it's released!",
+        "image": "rezzed-2017/yooka-laylee.jpg"
+    },
+    {
+        "day": "Saturday",
+        "timestart": "16:00",
+        "timeend": "16:30",
+        "heading": "Halo Wars 2: How we made a game for everyone",
+        "subheading": "Cassie Morgan and Dan Brewer from Creative Assembly will talk about the challenges involved in designing for both hardcore RTS fans but also for players new to RTS games.",
+        "image": "rezzed-2017/halo-wars.jpg"
+    },
+    {
+        "day": "Saturday",
+        "timestart": "17:00",
+        "timeend": "17:30",
+        "heading": "How 1968 Shaped the Characters and World of Mafia III",
+        "subheading": "Bill Harms, Narrative Director at Hangar 13 and Lead Writer of Mafia III, discusses the challenges involved in creating a video game set in New Bordeaux, a fictional version of 1968 New Orleans.",
+        "image": "rezzed-2017/mafia.jpg"
+    }
+];
